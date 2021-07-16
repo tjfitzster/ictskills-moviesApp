39699107358e2d8dcb6bf3f677db9a61126a6d75
tjfitzzster <tjfitzster@gmail.com>
2021-07-16 22:28:34 +0100

    Full review page

--- src/index.js
+++ src/index.js
@@ -3,23 +3,21 @@ import ReactDOM from "react-dom";
 import HomePage from "./pages/homepage";
 import MoviePage from "./pages/movieDetailsPage";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
-import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
+import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; 
+import SiteHeader from './components/siteHeader'
+import MovieReviewPage from "./pages/movieReviewPage";
+import upcomingMoviesPage from "./pages/upcomingMoviesPage"; 
 
 const App = () => {
   return (
     <BrowserRouter>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/movies/favorites">Favorites</Link>
-      </li>
-    </ul>
+    <SiteHeader />      {/* New Header  */}
     <Switch>
       <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+      <Route exact path="/movies/upcoming" component={upcomingMoviesPage} />
       <Route path="/movies/:id" component={MoviePage} />
       <Route exact path="/" component={HomePage} />
+      <Route path="/reviews/:id" component={MovieReviewPage} />
       <Redirect from="*" to="/" />
     </Switch>
   </BrowserRouter>
