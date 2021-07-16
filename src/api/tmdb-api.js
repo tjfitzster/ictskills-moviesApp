8eb4ab284e31540a5af8b98ca30311c9e6c4d764
tjfitzzster <tjfitzster@gmail.com>
2021-07-16 22:03:51 +0100

    Show movie review excerpts; Refactor Details page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -29,3 +29,23 @@ export const getMovies = () => {
       .then((res) => res.json())
       .then((json) => json.posters);
   };
+
+  export const getUpcomingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
+  };
+
+  export const getMovieReviews = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json.results);
+        return json.results;
+      });
+
+    };
